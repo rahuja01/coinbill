f596fa4bdcde54a1a67c178f7dcb76b930ce3487
rohitahuja <rohitahuja123@gmail.com>
2022-02-12 00:03:28 -0500

    second commit for Contoller handle case implementation

--- src/main/java/com/bill/coinbill/controller/CoinController.java
+++ src/main/java/com/bill/coinbill/controller/CoinController.java
@@ -3,6 +3,9 @@ package com.bill.coinbill.controller;
 import com.bill.coinbill.entity.CoinSchema;
 import com.bill.coinbill.service.CoinService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,14 +20,17 @@ public class CoinController {
     @Autowired
     CoinService coinService;
 
-    @GetMapping("/{bill}")
-    public List<CoinSchema> getCoin(@PathVariable Integer bill){
+    @GetMapping(path = "/{bill}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getCoin(@PathVariable Integer bill){
 
         List<CoinSchema> coinSchemasList = coinService.getCoin(bill);
+        if(coinSchemasList!=null){
+            return new ResponseEntity<String>(coinSchemasList.toString(), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<String>("There is no available coins for the bill provided, please try again later ",
+                    HttpStatus.NOT_FOUND);
+        }
 
-        return coinSchemasList;
     }
-
-
 
 }
